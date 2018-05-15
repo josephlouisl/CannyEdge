9f3bbd8c737cbab7973b58734aa631d4a156ed32
josephlouisl <36812861+josephlouisl@users.noreply.github.com>
2018-05-15 23:33:24 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 MEDIA_ROOT = 'media/'
 PRECESS_IMG_QUEUE = 'process_img_queue'
-#RABBITMQ_HOST = 'amqp://guest:guest@rabbit/'
-RABBITMQ_HOST = 'amqp://guest:guest@127.0.0.1/'
+RABBITMQ_HOST = 'amqp://guest:guest@rabbit/'
+#RABBITMQ_HOST = 'amqp://guest:guest@127.0.0.1/'
 
 
 MC = {
